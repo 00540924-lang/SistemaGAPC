e6f805b750fc6ca7e8ec618ffe6668a8599f42e7
Reyes <00540924@uca.edu.sv>
2025-11-15 20:06:06 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -12,7 +12,7 @@ def verificar_usuario(Usuario, Contraseña):
 
     try:
         cursor = con.cursor()
-        query = "SELECT Usuario, Contraseña FROM Usuarios WHERE Usuario = %s AND Contraseña = %s"
+        query = "SELECT Usuario, Contraseña FROM Administrador WHERE Usuario = %s AND Contraseña = %s"
         cursor.execute(query, (Usuario, Contraseña))
         result = cursor.fetchone()
         return result[0] if result else None
