fb56e33dcfe459dd53cab60e7671f95bd1046ce9
llyescas <00541424@uca.edu.sv>
2025-11-16 01:47:00 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -81,7 +81,7 @@ def login():
         """
         <div style="
             background: linear-gradient(135deg, #B7A2C8, #F7C9A4);
-            padding: 25px;
+            padding: 5px;
             border-radius: 12px;
             color: #ffffff;
             font-size: 16px;
