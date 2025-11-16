2300018ed62b86f9604b5404bd932ad0ab39d5c0
llyescas <00541424@uca.edu.sv>
2025-11-15 23:57:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-# Asegurar que la ruta raíz esté en sys.path (necesario para Streamlit Cloud)
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-if ROOT_DIR not in sys.path:
-    sys.path.append(ROOT_DIR)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
 
 import streamlit as st
 from modulos.login import login
