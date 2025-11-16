09ea8760099fbf09b5333504a734aa35ad46746f
Reyes <00540924@uca.edu.sv>
2025-11-15 23:35:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from modulos.login import login
 # Comprobamos si la sesión ya está iniciada
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
 # Si la sesión está iniciada, mostrar el contenido de ventas
-
 else:
 # Si la sesión no está iniciada, mostrar el login
 login()
