6c40dc0fec465d9a620079b795e375e973d539df
Reyes <00540924@uca.edu.sv>
2025-11-15 23:33:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
+# app.py
 import streamlit as st
-from modulos.login import login
-from modulos.menu import mostrar_menu  # si tienes un menú separado
-
-def main():
-    if "sesion_iniciada" not in st.session_state:
-        st.session_state["sesion_iniciada"] = False
-
-    if st.session_state["sesion_iniciada"]:
-        mostrar_menu()   # <- Lo que viene después del login
-    else:
-        login()
-
-if __name__ == "__main__":
-    main()
-
+from modulos.login import login 
+# Llamamos a la función mostrar_venta para mostrar el mensaje en la app
+login()
