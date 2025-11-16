0492b33a3259dee4b48565d7e9d60b3b150eec01
Reyes <00540924@uca.edu.sv>
2025-11-15 23:36:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-# app.py
 import streamlit as st
-from modulos.login import login 
+from modulos.login import login
 
-# Comprobamos si la sesión ya está iniciada
-if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
-# Si la sesión está iniciada, mostrar el contenido de ventas
+# Inicializar variable si no existe
+if "sesion_iniciada" not in st.session_state:
+    st.session_state["sesion_iniciada"] = False
+
+# Si la sesión está iniciada
+if st.session_state["sesion_iniciada"]:
+    st.title("Bienvenido al sistema")
+    st.write("Contenido interno del sistema aquí...")
+
+# Si NO está iniciada
 else:
-# Si la sesión no está iniciada, mostrar el login
-login()
+    login()
+
