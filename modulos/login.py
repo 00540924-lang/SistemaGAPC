7aa2cf3707372b7f5ddaa0901b8cdd182e7605eb
llyescas <00541424@uca.edu.sv>
2025-11-16 01:18:10 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -34,7 +34,7 @@ def login():
     with col1:
         st.write("")
     with col2:
-        st.image("modulos/assets/logo_gapc.png", width=1200)
+        st.image("modulos/assets/logo_gapc.png", width=5000)
     with col3:
         st.write("")
 
