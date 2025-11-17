c06c7961af75507ab18f239c990f850cb84ea3e5
Reyes <00540924@uca.edu.sv>
2025-11-16 22:11:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from modulos.login import login
-from modulos.menu import menu_principal
+from modulos.menu import mostrar_menu
 
-if "sesion_iniciada" not in st.session_state:
-    st.session_state["sesion_iniciada"] = False
+if "logged" not in st.session_state:
+    st.session_state["logged"] = False
 
-if st.session_state["sesion_iniciada"]:
+if st.session_state["logged"]:
     mostrar_menu()
 else:
     login()
+
