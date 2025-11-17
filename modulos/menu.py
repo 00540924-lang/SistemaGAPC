f37939ebba85c1cc102c440bc1a710bd341efb8f
Reyes <00540924@uca.edu.sv>
2025-11-17 01:26:42 -0600

    Update menu.py

--- modulos/menu.py
+++ modulos/menu.py
@@ -96,11 +96,3 @@ div.stButton > button {
             st.session_state.clear()
             st.rerun()
 
-# -------- MOSTRAR CONTENIDO DEL MÓDULO --------
-from modulos.registrar_miembros import registrar_miembros
-
-if st.session_state["modulo"] == "registrar_miembros":
-    registrar_miembros()
-
-# Llamar la función
-mostrar_menu()
