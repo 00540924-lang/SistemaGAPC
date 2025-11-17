c54fb1ad8b9c903fd16a7fbef3dcf760850c9fc3
Reyes <00540924@uca.edu.sv>
2025-11-17 01:11:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from modulos.login import login
 from modulos.menu import mostrar_menu
 from modulos.paginas import cargar_pagina   # ← IMPORTANTE
 
+
 # ----- CONTROL DE SESIÓN -----
 if "sesion_iniciada" not in st.session_state:
     st.session_state["sesion_iniciada"] = False
@@ -13,6 +14,7 @@ if "modulo" not in st.session_state:
 
 # ----- APP -----
 if st.session_state["sesion_iniciada"]:
+    st.set_page_config(page_title="Sistema GAPC", layout="wide")
     # Mostrar menú y capturar el módulo seleccionado
     opcion = mostrar_menu()
 
