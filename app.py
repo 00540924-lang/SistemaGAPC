0c8d54c54364185ae262afe96dde2b71d3723a36
llyescas <00541424@uca.edu.sv>
2025-11-17 00:42:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
 from modulos.login import login
 from modulos.menu import mostrar_menu
+from modulos.paginas import cargar_pagina   # ← IMPORTANTE
 
+# ----- CONTROL DE SESIÓN -----
 if "sesion_iniciada" not in st.session_state:
     st.session_state["sesion_iniciada"] = False
 
+if "modulo" not in st.session_state: 
+    st.session_state["modulo"] = None
+
+
+# ----- APP -----
 if st.session_state["sesion_iniciada"]:
-    mostrar_menu()
+    # Mostrar menú y capturar el módulo seleccionado
+    opcion = mostrar_menu()
+
+    # Cargar la página correspondiente
+    if opcion:
+        cargar_pagina(opcion)
+
 else:
     login()
