921b216ba4d9b5c4eabd056dc219956bd73b8dac
Reyes <00540924@uca.edu.sv>
2025-11-16 21:58:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ from modulos.login import login
 from modulos.menu import menu_principal
 import streamlit as st
 
-if "usuario" not in st.session_state:
-    login()
-else:
+#if "usuario" not in st.session_state:
+    #login()
+#else:
     #dashboard()
+# Estado inicial
+if "sesion_iniciada" not in st.session_state:
+    st.session_state["sesion_iniciada"] = False
+if "modulo" not in st.session_state:
+    st.session_state["modulo"] = None
+
+# Control de vistas
+if st.session_state["sesion_iniciada"]:
+    menu_principal()
+else:
+    login()
