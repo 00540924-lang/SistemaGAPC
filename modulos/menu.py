e92ebbed350af69e15e62cc62c317e3b20fbe561
Reyes <00540924@uca.edu.sv>
2025-11-17 00:52:28 -0600

    Update menu.py

--- modulos/menu.py
+++ modulos/menu.py
@@ -124,6 +124,11 @@ st.markdown("""
             st.session_state.clear()
             st.rerun()
 
+# -------- MOSTRAR CONTENIDO DEL MÓDULO --------
+from modulos.registrar_miembros import registrar_miembros
+
+if st.session_state["modulo"] == "registrar_miembros":
+    registrar_miembros()
 
 # Llamar la función
 mostrar_menu()
