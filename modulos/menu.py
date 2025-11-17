200b2ac141a01e03a00841a48fb3c9bd9c538ab7
Reyes <00540924@uca.edu.sv>
2025-11-17 02:05:03 -0600

    Update menu.py

--- modulos/menu.py
+++ modulos/menu.py
@@ -97,10 +97,4 @@ div.stButton > button {
             st.session_state.clear()
             st.rerun()
 
-# -------- MOSTRAR CONTENIDO DEL MÓDULO --------
-from modulos.registrar_miembros import registrar_miembros
-
-if st.session_state["modulo"] == "registrar_miembros":
-    registrar_miembros()
-
 
