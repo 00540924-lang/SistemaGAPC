0064a9bf4af6ec954844c7b6f3eed60bee683c0e
Reyes <00540924@uca.edu.sv>
2025-11-17 02:08:20 -0600

    Update menu.py

--- modulos/menu.py
+++ modulos/menu.py
@@ -77,7 +77,7 @@ div.stButton > button {
     st.markdown("""
         <div class='cards-row'>
             <div class='card g1'>📁<div class='card-sub'>Gestión de Proyectos</div></div>
-            <div class='card g1'>👥<div class='card-sub'>Registrar miembros</div></div>
+            <div class='card g2'>👥<div class='card-sub'>Registrar miembros</div></div>
             <div class='card g3'>🧾<div class='card-sub'>Inspecciones y Evaluaciones</div></div>
             <div class='card g4'>📄<div class='card-sub'>Gestión Documental</div></div>
             <div class='card g5'>📊<div class='card-sub'>Reportes</div></div>
