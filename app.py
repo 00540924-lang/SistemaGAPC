8f881390a09062e3037d6733621d9eb745f182e3
Reyes <00540924@uca.edu.sv>
2025-11-16 21:56:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from modulos.login import login
-from modulos.dashboard import dashboard
+#from modulos.dashboard import dashboard
+from modulos.menu import menu_principal
 import streamlit as st
 
 if "usuario" not in st.session_state:
