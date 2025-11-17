fc8dbbe4f345bc3cab142dfe95b8d988e312d988
Reyes <00540924@uca.edu.sv>
2025-11-17 00:54:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
 import streamlit as st
-from modulos.login import login
 from modulos.menu import mostrar_menu
-from modulos.paginas import cargar_pagina   # ← IMPORTANTE
 
-# ----- CONTROL DE SESIÓN -----
-if "sesion_iniciada" not in st.session_state:
-    st.session_state["sesion_iniciada"] = False
+st.set_page_config(page_title="Sistema GAPC", layout="wide")
 
-if "modulo" not in st.session_state: 
-    st.session_state["modulo"] = None
+mostrar_menu()
 
-
-# ----- APP -----
-if st.session_state["sesion_iniciada"]:
-    # Mostrar menú y capturar el módulo seleccionado
-    opcion = mostrar_menu()
-
-    # Cargar la página correspondiente
-    if opcion:
-        cargar_pagina(opcion)
-
-else:
-    login()
