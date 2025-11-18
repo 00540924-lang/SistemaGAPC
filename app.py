d3c7e3ca009d4a3307d159f977107b0fda7aab9e
Reyes <00540924@uca.edu.sv>
2025-11-17 19:49:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,27 +31,3 @@ elif pagina == "registrar_miembros":
     from modulos.registrar_miembros import registrar_miembros
     registrar_miembros()
 
-# ---- AGREGAR AQUÍ TUS OTROS MÓDULOS ----
-elif pagina == "proyectos":
-    from modulos.proyectos import proyectos
-    proyectos()
-
-elif pagina == "usuarios":
-    from modulos.usuarios import usuarios
-    usuarios()
-
-elif pagina == "inspecciones":
-    from modulos.inspecciones import inspecciones
-    inspecciones()
-
-elif pagina == "documentos":
-    from modulos.documentos import documentos
-    documentos()
-
-elif pagina == "reportes":
-    from modulos.reportes import reportes
-    reportes()
-
-elif pagina == "configuracion":
-    from modulos.configuracion import configuracion
-    configuracion()
