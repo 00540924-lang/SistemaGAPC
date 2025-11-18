0176eaf7b9a5082882a53aee4c3613d33eed36b0
Reyes <00540924@uca.edu.sv>
2025-11-17 21:29:26 -0600

    Update registrar_miembros.py

--- modulos/registrar_miembros.py
+++ modulos/registrar_miembros.py
@@ -46,7 +46,7 @@ def registrar_miembros():
             database="bzn5gsi7ken7lufcglbg"
         )
         cursor = conexion.cursor()
-        cursor.execute("SELECT id_miembros, Nombre, DUI, Telefono FROM Miembros")
+        cursor.execute("SELECT Nombre, DUI, Telefono FROM Miembros")
         resultados = cursor.fetchall()
 
         if resultados:
