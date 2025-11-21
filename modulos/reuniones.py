ec07d01c9c60f9b811f367c95341e3f7b8fc2c2b
llyescas <00541424@uca.edu.sv>
2025-11-20 23:25:16 -0600

    Update reuniones.py

--- modulos/reuniones.py
+++ modulos/reuniones.py
@@ -38,7 +38,7 @@ def mostrar_reuniones(id_grupo):
     with st.container():
         st.markdown(
             """
-            <div style='background-color:#4C3A60; padding:14px; border-radius:12px; box-shadow: 0 4px 12px rgba(0,0,0,0.1);'>
+            <div style='background-color:#4C3A60; padding:8px; border-radius:12px; box-shadow: 0 4px 12px rgba(0,0,0,0.1);'>
         """,
             unsafe_allow_html=True
         )
